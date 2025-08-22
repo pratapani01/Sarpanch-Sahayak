56b8e89270d1277dae8e4bd535d7458998f3de27
pratapani01 <pratapani01@gmail.com>
2025-08-22 15:36:10 +0530

    feat: update API URL for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an instance of axios
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Your backend server URL
+  baseURL: 'https://sarpanch-sahayak-api.onrender.com', // Your backend server URL
   headers: {
     'Content-Type': 'application/json',
   },
